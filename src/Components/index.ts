e666a5dcf8d19a6c96d5b746d97fc925d3fe3404
selly361 <seikou615@gmail.com>
2024-06-08 15:44:27 +0100

    Export components

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,4 @@
 export { default as Hero } from './Hero'
 export { default as AnimeCard } from './AnimeCard'
 export { default as LoadMore } from './LoadMore'
+export { default as MotionDiv } from './MotionDiv'
