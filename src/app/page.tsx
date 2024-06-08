8d6070ae6c223b4625ffda10c9a828eaef8f3602
selly361 <seikou615@gmail.com>
2024-06-08 15:39:28 +0100

    Refactor with data from the api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 import { AnimeCard, LoadMore } from '@/Components'
-import { data } from './_data'
-import { Anime } from '@/Types'
+import { fetchAnimes } from './action'
 
 async function Home() {
+	const data = await fetchAnimes(1)
 	return (
 		<main className='sm:p-16 py-16 px-8 flex flex-col gap-10'>
-			<h2 className='text-3xl text-white font-bold'>Explore Anime</h2>
+			<h2 className='text-3xl text-white font-bold'>Explore Animes</h2>
 
 			<section className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10'>
-				{data.map((item: Anime, index) => (
-					<AnimeCard key={item.id} anime={item} index={index} />
-				))}
+				{data}
 			</section>
 			<LoadMore />
 		</main>
